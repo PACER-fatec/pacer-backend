0905df339fccde4919db4de97f0e3e8c836f5289
Andre Pires <andre.pires2@fatec.sp.gov.br>
2021-11-01 23:09:20 -0300

    PCFTC-55 Fix path

--- pacer_fatec/importDb.py
+++ pacer_fatec/importDb.py
@@ -2,10 +2,12 @@ import json
 import pymongo
 import mdb_connection as mdb
 import csv
+import pacer as pc
 myclient = pymongo.MongoClient()
 
+
 def importAlunos():
-    with open('pacer_fatec/resources/alunos.csv', 'r') as read_csv:
+    with open(f'{pc.RES_DIR}//alunos.csv', 'r') as read_csv:
         csv_reader = csv.DictReader(read_csv)
         listaAlunos = list(csv_reader)
         mdb.db.alunos.insert_many(listaAlunos)
