2881716352fd2a3a5d5d478a14685aa2d3bfc993
wesleyvs <wesleyvini.social@gmail.com>
2022-08-24 01:36:41 -0300

    CORS solving

--- pacer_fatec/pacer.py
+++ pacer_fatec/pacer.py
@@ -44,9 +44,11 @@ def enviarAvaliacao ():
     return mensagem
 
 @app.route('/pacer/cadastro', methods = ['POST'])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin()
 def cadastro ():
     requestDict = request.json
+
+    requestDict.headers.add('Access-Control-Allow-Origin', '*')
 
     print(requestDict)
 
