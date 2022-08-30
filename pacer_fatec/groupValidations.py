ff8483f01f78b55de3e829ac9109f1dbaa546fb2
wesleyvs <wesleyvini.social@gmail.com>
2022-08-30 00:32:44 -0300

    PCRFTC-45 validações finalizadas

---
+++ pacer_fatec/groupValidations.py
@@ -0,0 +1,24 @@
+from asyncio.windows_events import NULL
+from pickle import FALSE, TRUE
+from queue import Empty
+from bson import json_util, ObjectId
+import mdb_connection as mdb
+
+def existe_alunos(alunos):
+
+    erro = FALSE
+
+    if not alunos:
+        mensagem = "ERRO: Nenhum aluno encontrado"
+    for aluno in alunos:
+        if not mdb.db.users.find_one({'email': aluno}):
+            mensagem = "ERRO: " + aluno + " não encontrado na base de dados!"
+            return mensagem
+
+    return NULL
+
+def existe_grupo(nomeGrupo):
+    if mdb.db.grupos.find_one({'nome': nomeGrupo}):
+        return "ERRO: Nome de grupo já utilizado!"
+        
+    return NULL
