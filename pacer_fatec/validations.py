8c540d9c0320a9e3b4a2f2c06b1d269b49d9e7d2
wesleyvs <wesleyvini.social@gmail.com>
2023-06-23 01:55:30 -0300

    PCRFTC-68 alterações na avaliação para aceitar novo formulário

--- pacer_fatec/validations.py
+++ pacer_fatec/validations.py
@@ -2,11 +2,16 @@ import mdb_connection as mdb
 
 def aluno_pode_avaliar(json):
     avaliador = json['avaliador']
-    avaliado = json['avaliado']
     sprint = json['sprint']
-    avaliacoes_existentes = mdb.db.fatec.find({ 'avaliador': avaliador, 'avaliado': avaliado, 'sprint': sprint}).count()
-    if avaliacoes_existentes > 0:
-        return False
+    grupo = json['nomeGrupo']
+    avaliacoes = json['avaliacoes']
+
+    for avaliacao in avaliacoes:
+        avaliado = avaliacao['avaliado']
+        avaliacoes_existentes = mdb.db.fatec.find({'avaliador': avaliador, 'sprint': sprint, 'nomeGrupo': grupo}).count()
+        if avaliacoes_existentes > 0:
+            return False
+
     return True
 
 def existe_cadastro(email):
